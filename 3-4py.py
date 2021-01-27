d4935b58b61ea14c2c59bdb85360b4869672000b
chiu-eason <77959636+chiu-eason@users.noreply.github.com>
2021-01-27 16:45:25 +0800

    Add files via upload

---
+++ 3-4py.py
@@ -0,0 +1,23 @@
+
+v
+
+while True:
+    hits = mc.events.pollProjectileHits()
+    if len(hits) > 0 :
+        a = hits[0]
+        x,y,z = a.pos.x,a.pos.y,a.pos.z
+        mc.player.setTilePos(x,y,z)
+        mc.spawnEntity(x,y,z,99)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
